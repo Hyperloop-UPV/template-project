088f974f14dbefa5c32517d99d13d48c2091771b
Jorge Sáez <jorgeesg82@gmail.com>
2026-02-08 15:00:18 +0100

    Added ADC and Ethernet examples and removed old VirtualMCU. Also added test utilities

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,29 +1,25 @@
 #include "main.h"
-
 #include "ST-LIB.hpp"
 
 using namespace ST_LIB;
 
-constexpr auto led = ST_LIB::DigitalOutputDomain::DigitalOutput(ST_LIB::PF13);
+constexpr auto led = ST_LIB::DigitalOutputDomain::DigitalOutput(ST_LIB::PB0);
 
 using MainBoard = ST_LIB::Board<led>;
 
-#if !defined(EXAMPLE_ADC) && !defined(EXAMPLE_ETHERNET) && !defined(EXAMPLE_MPU) &&                \
-    !defined(EXAMPLE_HARDFAULT)
 int main(void) {
-    MainBoard::init();
+  MainBoard::init();
 
-    auto& led_instance = MainBoard::instance_of<led>();
+  auto &led_instance = MainBoard::instance_of<led>();
 
-    while (1) {
-        led_instance.toggle();
-        HAL_Delay(200);
-    }
+  while (1) {
+    led_instance.toggle();
+    HAL_Delay(200);
+  }
 }
-#endif
 
 extern "C" void Error_Handler(void) {
-    ErrorHandler("HAL error handler triggered");
-    while (1) {
-    }
+  ErrorHandler("HAL error handler triggered");
+  while (1) {
+  }
 }
