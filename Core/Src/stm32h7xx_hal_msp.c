b4312d14e8cf1d7c6e6f7abce6a4aee8ec5c8a18
Jorge Sáez <jorgeesg82@gmail.com>
2026-02-14 12:39:50 +0100

    fix(spi): declare SPI3 DMA handles in MSP

--- Core/Src/stm32h7xx_hal_msp.c
+++ Core/Src/stm32h7xx_hal_msp.c
@@ -30,6 +30,8 @@ extern DMA_HandleTypeDef hdma_i2c2_tx;
 extern DMA_HandleTypeDef hdma_fmac_preload;
 extern DMA_HandleTypeDef hdma_fmac_read;
 extern DMA_HandleTypeDef hdma_fmac_write;
+extern DMA_HandleTypeDef hdma_spi3_rx;
+extern DMA_HandleTypeDef hdma_spi3_tx;
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN TD */
