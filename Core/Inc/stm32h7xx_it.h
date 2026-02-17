571280bc0b471f054231c5058fae355e1038cc7c
victhor <victorlopezcortes25@gmail.com>
2026-02-17 19:21:58 +0100

    Remove from stm32h7xx_it.h diff --git a/Core/Inc/stm32h7xx_it.h b/Core/Inc/stm32h7xx_it.h index 19b0972..f14abd9 100644 --- a/Core/Inc/stm32h7xx_it.h +++ b/Core/Inc/stm32h7xx_it.h @@ -55,18 +55,6 @@ void SVC_Handler(void);  void DebugMon_Handler(void);  void PendSV_Handler(void);  void SysTick_Handler(void); -void EXTI0_IRQHandler(void); -void EXTI1_IRQHandler(void); -void DMA1_Stream0_IRQHandler(void); -void DMA1_Stream1_IRQHandler(void); -void DMA1_Stream2_IRQHandler(void); -void DMA1_Stream3_IRQHandler(void); -void DMA1_Stream4_IRQHandler(void); -void DMA1_Stream5_IRQHandler(void); -void DMA1_Stream6_IRQHandler(void); -void DMA2_Stream0_IRQHandler(void); -void DMA2_Stream1_IRQHandler(void); -void DMA2_Stream2_IRQHandler(void);  void FMAC_IRQHandler(void);  void I2C2_EV_IRQHandler(void);  void SPI3_IRQHandler(void); @@ -74,8 +62,6 @@ void ETH_IRQHandler(void);  void LPTIM1_IRQHandler(void);  void LPTIM2_IRQHandler(void);  void LPTIM3_IRQHandler(void); -void TIM23_IRQHandler(void); -void TIM24_IRQHandler(void);  /* USER CODE BEGIN EFP */
     /* USER CODE END EFP */

--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -55,18 +55,6 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
-void EXTI0_IRQHandler(void);
-void EXTI1_IRQHandler(void);
-void DMA1_Stream0_IRQHandler(void);
-void DMA1_Stream1_IRQHandler(void);
-void DMA1_Stream2_IRQHandler(void);
-void DMA1_Stream3_IRQHandler(void);
-void DMA1_Stream4_IRQHandler(void);
-void DMA1_Stream5_IRQHandler(void);
-void DMA1_Stream6_IRQHandler(void);
-void DMA2_Stream0_IRQHandler(void);
-void DMA2_Stream1_IRQHandler(void);
-void DMA2_Stream2_IRQHandler(void);
 void FMAC_IRQHandler(void);
 void I2C2_EV_IRQHandler(void);
 void SPI3_IRQHandler(void);
@@ -74,8 +62,6 @@ void ETH_IRQHandler(void);
 void LPTIM1_IRQHandler(void);
 void LPTIM2_IRQHandler(void);
 void LPTIM3_IRQHandler(void);
-void TIM23_IRQHandler(void);
-void TIM24_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
